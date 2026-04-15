19960a39d4dc0425d6bf6d4f2f0056def5b0f84f
LanikaiLi <phoenix.li@getmaintainx.com>
2026-04-14 20:59:10 -0400

    a server that can handle GET Request

--- 4-Backend-Dev/movie-watchlist-server/readme.md
+++ 4-Backend-Dev/movie-watchlist-server/readme.md
@@ -7,3 +7,9 @@ to setup a folder to work on backend, you need to :
 don't forget to gitignore node_modules and package-lock.json, so if someone downloads your code, they can just run npm install to start working
 
 it is suggested to change the main from index.js to main.js in the package.json file, index.js is the default one machine generated
+
+## server.js breakdown
+
+`const express = require('express')` — imports the Express library into the file. `require()` is how Node.js loads installed packages from node_modules.
+
+`const app = express()` — calls the express function to create a new application instance. This `app` object is what you use to define routes, set up middleware, and start the server.
---
+++ 4-Backend-Dev/movie-watchlist-server/server.js
@@ -0,0 +1,38 @@
+const express = require('express') // imports the Express library into the file. `require()` is how Node.js loads installed packages from node_modules.
+
+const app = express() // calls the express function to create a new application instance. This `app` object is what you use to define routes, set up middleware, and start the server.
+
+const movies = [
+    {title: 'Mario Galaxy', starring: ['Chris Pratt', 'Anya Taylor Joy']},
+    {title: 'The Drama', starring: ['Zendaya', 'Robert Pattinson']},
+    {title: 'Scream 7', starring: ['Neve Campbell', 'Jasmin Savoy Brown']},
+    {title: 'You Me & Tuscany', starring: ['Regé-Jean Page', 'Halle Bailey']},
+    {title: 'Nirvana the Band the Show the Movie', starring: ['Matt Johnson', 'Jay McCarrol']},
+    {title: 'Hamnet', starring: ['Jessie Buckley', 'Paul Mescal']}
+  ]
+
+  app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to the movie watchlist server." // for now req is not important, it will be used when the user send a POST request to the server, but now it is just a GET request to the server
+    })
+})
+
+app.get("/movies", (req, res) => {
+    res.json(movies)
+})
+
+app.get("/movies/:name", (req, res) => {
+    console.log(req.params.name)
+    const selectedMovie = movies.find(movie => movie.title === req.params.name)
+    res.json(selectedMovie) 
+})
+
+// exercise: try to build a route to find all movies starring a specific actor
+
+
+const port = 3000 // this is the port number that the server will listen on
+app.listen(port, () => { // remember, app.listen has to be put after all the routes are defined, if you put it before routes (app.get("/movies", (req, res) => { ... })), the server will not start
+    console.log(`Server is running on port ${port}`)
+})
+
+
