df2b2ca533041a57660a43ea3aa5d06affece6e3
LanikaiLi <phoenix.li@getmaintainx.com>
2026-05-29 08:56:29 -0400

    ...

---
+++ 4-Backend-Dev/warmup-12/script.js
@@ -0,0 +1,19 @@
+// Warmup for Thursday: HTML Form
+// Create an HTML form that lets a user enter:
+// Their name
+// Their favorite color
+// On submit, log their name to the console.
+// Bonus: change the page background to their favorite color.
+
+// my logic: i have he form in a separate html file, i only need to use this code to get the input name and favorite color, then log the name to the console, and change the page background to the favorite color.
+
+// TODO: watch 7:00-7:10 PM recording, talks about how to get the input value from the event instead of the element's value. 
+
+const nameForm = document.getElementById("name-form")
+nameForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(event)
+    const name = document.getElementById("name").value
+    const favoriteColor = document.getElementById("favorite-color").value
+    console.log(name)
+})
