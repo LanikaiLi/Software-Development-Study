b94aa337e83b53054a80ac8a9bfd5dbf51f7d4be
LanikaiLi <phoenix.li@getmaintainx.com>
2026-06-02 21:14:43 -0400

    add more api methods to employee route

--- 4-Backend-Dev/mongoose-project/src/routes/employees.js
+++ 4-Backend-Dev/mongoose-project/src/routes/employees.js
@@ -8,4 +8,21 @@ router.post('/', async (req, res) => {
     res.status(201).json(newEmployee);
 })
 
+router.get('/', async (req, res) => {
+	const employees = await Employee.find()
+	res.json(employees)
+})
+
+router.get('/:id', async (req, res) => {
+	const employee = await Employee.findById(req.params.id)
+	if (!employee) return res.status(404).json({message: 'Employee not found'})
+	res.json(employee)
+})
+
+router.delete('/:id', async (req, res) => {
+	const employee = await Employee.findByIdAndDelete(req.params.id)
+	if (!employee) return res.status(404).json({message: 'Employee not found'})
+	res.json({ message: 'Employee deleted successfully' })
+})
+
 module.exports = router; // this is to export the router so we can use it in other files
