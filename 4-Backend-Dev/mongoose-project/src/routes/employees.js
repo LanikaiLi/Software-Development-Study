ea4067122507e569c36d30ef7cbb9c59600bb007
LanikaiLi <phoenix.li@getmaintainx.com>
2026-06-04 21:04:55 -0400

    learn writing queries with mongoose to query specific data

--- 4-Backend-Dev/mongoose-project/src/routes/employees.js
+++ 4-Backend-Dev/mongoose-project/src/routes/employees.js
@@ -25,4 +25,54 @@ router.delete('/:id', async (req, res) => {
 	res.json({ message: 'Employee deleted successfully' })
 })
 
+//more Mongoose queries:
+
+// ---- SORTING ----
+// 1. sort employees by dateJoined in descending order
+router.get('/sorted/date-joined', async(req, res) => {
+	const employees = await Employee.find().sort({dateJoined: -1}) // -1 is descending order, 1 is ascending order, .sort() is a Mongoose method which is like ORDER BY in SQL, you need to provide the column you want to sort by, and the order you want to sort by, in this case, we want to sort by dateJoined in descending order
+	res.json(employees)
+})
+
+//2. sort employees alphabetically by last name
+router.get('/sorted/last-name', async(req,res) => {
+	const employees = await Employee.find().sort({lastName: 1})
+	res.json(employees)
+})
+
+// ----- FILTERING ------
+//3. find employees with a single specific certification
+router.get('/certification/:cert', async(req, res) => {
+	const employees = await Employee.find({certifications: req.params.cert})
+	res.json(employees)
+})
+
+//4. Find employees with multiple specific certifications
+//query string is a part of URL that assigns values to specific parameters, it's a common used pattern for filtering data, example: http://localhost:3000/employees/multiple/certifications?list=cpr,welding, "?list=cpr,welding" is the query string
+router.get('/multiple/certifications', async(req, res) => {
+	const certs = req.query.list.split(',')
+	const employees = await Employee.find({certifications: {$all: certs}})
+	res.json(employees)
+})
+
+//5. Find employees with some certifications who are available on specific days
+router.get('/availability/and/certifications', async(req, res) => {
+	//console.log(req.query.certs)
+	const certs = req.query.certs.split(',')
+	const days = req.query.availability.split(',')
+	const employees = await Employee.find({certifications: {$all: certs}, availability: {$all: days}})
+	res.json(employees)
+})
+
+//6. Find employees with some certifications who are available on specific days and have joined the company for more than X months
+router.get('/availability/and/certifications/and/months', async(req, res) => {
+	//console.log(req.query)
+	const certs = req.query.certs.split(',')
+	const days = req.query.availability.split(',')
+	const months = req.query.months
+	//console.log(new Date(Date.now() - months * 30 * 24 * 60 * 60 * 1000))
+	const employees = await Employee.find({certifications: {$all: certs}, availability: {$all: days}, dateJoined: {$lte: new Date(Date.now() - months * 30 * 24 * 60 * 60 * 1000)}}) // $gt is greater than, $lt is less than, $gte is greater than or equal to, $lte is less than or equal to
+	res.json(employees)
+})
+
 module.exports = router; // this is to export the router so we can use it in other files
