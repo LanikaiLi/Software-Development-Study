4330030176ae0b299374419df00db5b6c8a431be
LanikaiLi <phoenix.li@getmaintainx.com>
2026-07-07 20:39:10 -0400

    serve static files like images, this is needed because we are displaying frontend on server directly with ejs tech stack and we need to make sure once we deploy server to render, the static files are still accessible

--- 7-Full-Stack-Integration/book-inventory/server.js
+++ 7-Full-Stack-Integration/book-inventory/server.js
@@ -5,6 +5,8 @@ const path = require('path'); // this is the path module, it is used to join the
 const PORT = 3000;
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public'))); // this is used to serve static files like images, css, js, etc.
+
 app.get('/', (req, res) => {
     res.render('index.ejs', { books: books });
 })
