d09778104c56d89a92cc938c49a2589eb09dd8d4
LanikaiLi <phoenix.li@getmaintainx.com>
2026-07-14 20:23:12 -0400

    implement api logics

--- Assignments/task_management_app/server.js
+++ Assignments/task_management_app/server.js
@@ -1,10 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 
 const app = express()
 app.use(express.json())
 
+// MongoDB connection
 let db
 
 async function connectDB() {
@@ -24,3 +25,40 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 export const getDB = () => db
+
+ // api logics
+
+ // 1. create a task
+app.post('/tasks', async (req, res) => {
+    const { title, description, priority, status, deadline } = req.body
+  
+    const task = {
+      title,
+      description,
+      priority,
+      status,
+      deadline,
+      created_at: new Date()
+    }
+  
+    const result = await getDB().collection('tasks').insertOne(task)
+  
+    res.status(201).json({ message: 'Task created', task: { ...task, _id: result.insertedId } })
+  })
+
+// 2. update a task
+app.patch('/tasks/:id', async (req, res) => {
+    const id = req.params.id
+    const updates = req.body
+  
+    // find the task and update it
+    const result = await getDB().collection('tasks').findOneAndUpdate(
+      { _id: new ObjectId(id) },  // use objectid to change the type of the var from string to objectid, because mongodb uses objectid for the id, but postman sends the id as a string
+      { $set: updates },           // set: only update the fields that are provided in the request body
+      { returnDocument: 'after' } // return the updated document
+    )
+  
+    if (!result) return res.status(404).json({ error: 'Task not found' })
+  
+    res.json({ message: 'Task updated', task: result })
+  })
