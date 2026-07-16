7027a11698d101e610faea1b08091aa377e45785
LanikaiLi <phoenix.li@getmaintainx.com>
2026-07-16 19:08:06 -0400

    add one more unit test

--- 8-Software-Testing/unit-testing/tests/script.test.js
+++ 8-Software-Testing/unit-testing/tests/script.test.js
@@ -5,4 +5,9 @@ describe('Tests for script.js functions', () => {
     it('should reverse an array', () => {
         expect(reverseArray([1, 2, 3, 4])).to.deep.equal([4, 3, 2, 1]) // need to use deep because js by nature does not agree that [1,2,3] == [1,2,3], js is a wierd language
     })
+
+    it('removeDuplicates returns a new array with duplicates removed', () => {
+        expect(removeDuplicates([1, 2, 2, 3, 4, 4, 5])).to.deep.equal([1, 2, 3, 4, 5])
+    })
+
 })
